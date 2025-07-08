46df5c5b0e2dd53c836ab1c9209df585c11a934b
MoimoiOD <moi.oli.dua.sei@gmail.com>
2025-07-08 13:40:31 -0400

    atualização do componente raiz para receber os novos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header.component';
+import { SidebarComponent } from './layout/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'help_desk';
+  title = 'HelpDesk Pro';
 }
