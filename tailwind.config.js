22765adaad94bd4f1f0bc3c62655c0faa551a6cc
MoimoiOD <moi.oli.dua.sei@gmail.com>
2025-07-08 13:05:03 -0400

    correção de erro de configuração no postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
@@ -81,4 +79,4 @@ module.exports = {
   },
   plugins: [],
   darkMode: 'class',
-}
+};
