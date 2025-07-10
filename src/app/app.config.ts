3f29ce79a108e66618e6a21b377e6116db0e6c3a
MoimoiOD <moi.oli.dua.sei@gmail.com>
2025-07-10 08:48:24 -0400

    Atualizando as configurações de rota

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { authInterceptor, headerInterceptor, loggingInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        headerInterceptor,
+        authInterceptor,
+        loggingInterceptor
+      ])
+    )
+  ]
 };
