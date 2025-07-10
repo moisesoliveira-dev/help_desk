c213ce07c90eaf20f206edc621155873d2d28d83
MoimoiOD <moi.oli.dua.sei@gmail.com>
2025-07-10 08:28:59 -0400

    Atualizando as rotas para incluir autenticação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,39 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard.component';
 import { ThemeShowcaseComponent } from './features/theme-showcase/theme-showcase.component';
+import { AuthLayoutComponent } from './features/auth/auth-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            },
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            },
+            {
+                path: 'forgot-password',
+                component: ForgotPasswordComponent
+            }
+        ]
     },
     {
         path: 'dashboard',
